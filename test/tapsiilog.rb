7af5fac8d9c728590bec10e17eebd6e087d311b8
Majid Arif Siddiqui <me@majidarif.com>
2014-05-01 09:17:04 +0800

    added initial testing

--- test/tapsiilog.rb
+++ test/tapsiilog.rb
@@ -1,4 +1,4 @@
-require './lib/palmade/tapsilog'
+require '../lib/palmade/tapsilog'
 
 logger = Palmade::Tapsilog::Logger.new('default', '/tmp/tapsilog.sock', 'some_serious_key')
 logger.level = Palmade::Tapsilog::Logger::DEBUG 
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,2 +1,34 @@
-require 'rubygems'
+# require 'rubygems'
+require '../lib/palmade/tapsilog'
 require 'test/unit'
+
+class TestTapsilog < Test::Unit::TestCase
+  
+  def setup
+    @logger = Palmade::Tapsilog::Logger.new('default', '/tmp/tapsilog.sock', 'some_serious_key')
+  end
+  
+  def teardown
+    ## Nothing really
+  end
+  
+  def test_no_fail
+    assert_nothing_raised( RuntimeError ) { @logger.info("I am logging a message.", {:author => "arif"}) }
+  end
+  
+  def test_srv_alive
+    ## todo
+  end
+
+  ## add more test
+
+  protected 
+  
+  def db
+    if @db.nil?
+      @db = SQLite3::Database.new "logs.sqlite"
+    end
+    @db
+  end  
+  
+end
